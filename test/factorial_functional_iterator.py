23cc157e92168e859a5d31a9d376d6bd0231da48
Pablo.NA <90230330+Teetos1@users.noreply.github.com>
2022-12-20 00:38:07 -0600

    Delete factorial_functional_iterator.py

--- test/factorial_functional_iterator.py
+++
@@ -1,8 +0,0 @@
-from operator import mul
-from functools import reduce
-from factorial_check_input_value import checkInputValue
-
-
-@checkInputValue
-def factorial(number):
-    return reduce(mul, range(1, number + 1), 1)
